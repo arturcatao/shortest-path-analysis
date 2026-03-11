caac567f66b23ebdcc3bd6b94becdf46e228a303
malugomesp <malu.gomes439@gmail.com>
2026-03-11 06:31:03 -0300

    definitive results

--- src/main/java/benchmarks/DijkstraBenchmark.java
+++ src/main/java/benchmarks/DijkstraBenchmark.java
@@ -51,7 +51,7 @@ public class DijkstraBenchmark {
     //< PARÂMETROS DO EXPERIMENTO >
     // ---------------------------
     
-    @Param({"10", "30","50", "70", "90"})
+    @Param({"10", "30", "50", "70", "90"})
     private int density;
     
     // Número de vértices
